185feefcc8b2337889400db424b79c3b79b615bd
Patrícia Santana <patriciasan3@gmail.com>
2023-03-28 09:33:02 -0300

    acréscimo-de-ingredientes

--- ingredientes.js
+++ ingredientes.js
@@ -1,16 +1,184 @@
 let ingredientes = [        //array
     {                       //objeto
-        nome: 'Alface',        
-        valor: 2.5
+        nome: 'Abobóra',        
+        valorAdicional: 0.5,
+        img: 'img/abobora.png'
+    },
+    {                       
+        nome: 'Abobrinha',        
+        valorAdicional: 0.5,
+        img: 'img/abobrinha.png'
+    },
+    {                       
+        nome: 'Acelga',        
+        valorAdicional: 0.5,
+        img: 'img/acelga.png'
+    },
+    {                       
+        nome: 'Agrião',        
+        valorAdicional: 0.5,
+        img: 'img/agriao.png'
+    },
+    {                       
+        nome: 'Alface americana',        
+        valorAdicional: 0.5,
+        img: 'img/alface-americana.png'
+    },
+    {                       
+        nome: 'Alface crespa',        
+        valorAdicional: 0.5,
+        img: 'img/alface-crespa.png'
     },
     {
-        nome: 'Tomate',
-        valor: 3.5 
+        nome: 'Batata doce',
+        valorAdicional: 0.5,
+        img: 'img/batata-doce.png' 
     },
     {
         nome: 'Batata palha',
-        valor: 2 
-    }
+        valorAdicional: 0.5,
+        img: 'img/batata-palha.png' 
+    },
+    {
+        nome: 'Berinjela',
+        valorAdicional: 0.5,
+        img: 'img/berinjela.png' 
+    },
+    {
+        nome: 'Beterraba',
+        valorAdicional: 0.5,
+        img: 'img/beterraba.png'  
+    },
+    {
+        nome: 'Brócolis',
+        valorAdicional: 0.5,
+        img: 'img/brocolis.png'  
+    },
+    {
+        nome: 'Camarão',
+        valorAdicional: 0.5,
+        img: 'img/camarao.png'  
+    },
+    {
+        nome: 'carne',
+        valorAdicional: 0.5,
+        img: 'img/carne.png' 
+    },
+    {
+        nome: 'cebola-roxa',
+        valorAdicional: 0.5,
+        img: 'img/cebola-roxa.png' 
+    },
+    {
+        nome: 'cebolinha',
+        valorAdicional: 0.5,
+        img: 'img/cebolinha.png' 
+    },
+    {
+        nome: 'cenoura',
+        valorAdicional: 0.5,
+        img: 'img/cenoura.png' 
+    },
+    {
+        nome: 'chuchu',
+        valorAdicional: 0.5,
+        img: 'img/chuchu.png'  
+    },
+    {
+        nome: 'couve-flor',
+        valorAdicional: 0.5,
+        img: 'couve-flor.png'  
+    },
+    {
+        nome: 'croutonn',
+        valorAdicional: 0.5,
+        img: 'img/croutonn.png'
+    },
+    {
+        nome: 'espinafre',
+        valorAdicional: 0.5,
+        img: 'img/espinafre.png'
+    },
+    {
+        nome: 'frango',
+        valorAdicional: 0.5,
+        img: 'img/frango.png'
+    },
+    {
+        nome: 'limão',
+        valorAdicional: 0.5,
+        img: 'img/limao.png'
+    },
+    {
+        nome: 'molho-branco',
+        valorAdicional: 0.5,
+        img: 'img/molho-branco.png'
+    },
+    {
+        nome: 'molho-mostarda',
+        valorAdicional: 0.5,
+        img: 'img/molho-mostarda.png'
+    },
+    {
+        nome: 'molho-pimenta',
+        valorAdicional: 0.5,
+        img: 'img/molho-pimenta.png'
+    },
+    {
+        nome: 'molho-vermelho',
+        valorAdicional: 0.5,
+        img: 'img/molho-vermelho.png'
+    },
+    {
+        nome: 'pepino',
+        valorAdicional: 0.5,
+        img: 'img/pepino.png'
+    },
+    {
+        nome: 'queijo-ralado',
+        valorAdicional: 0.5,
+        img: 'img/queijo-ralado.png'
+    },
+    {
+        nome: 'repolho',
+        valorAdicional: 0.5,
+        img: 'img/repolho.png'
+    },
+    {
+        nome: 'repolho-roxo',
+        valorAdicional: 0.5,
+        img: 'img/repolho-roxo'
+    },
+    {
+        nome: 'rúcula',
+        valorAdicional: 0.5,
+        img: 'img/rucula.png'
+    },
+    {
+        nome: 'salmão',
+        valorAdicional: 0.5,
+        img: 'img/salmao.png'
+    },
+    {
+        nome: 'salsinha',
+        valorAdicional: 0.5,
+        img: 'img/salsinha.png'
+    },
+    {
+        nome: 'tilápia',
+        valorAdicional: 0.5,
+        img: 'img/tilapia.png'
+    },
+    {
+        nome: 'Tomate',
+        valorAdicional: 0.5,
+        img: 'img/tomate.png' 
+    },
+    {
+        nome: 'tomate-cereja',
+        valorAdicional: 0.5,
+        img: 'img/tomate-cereja.png'
+    },
 ]
 
 export default ingredientes; //somente o vetor
