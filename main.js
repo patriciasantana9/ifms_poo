9e4ad0457f09a4c6422c8b410148955e4e6c9740
Patrícia Santana <patriciasan3@gmail.com>
2023-03-28 11:06:45 -0300

    imagens-html

--- main.js
+++ main.js
@@ -2,4 +2,16 @@ import modificador from "./modificador.js";
 import ingredientes from  "./ingredientes.js";
 //import saladas from  "./saladas.js";
 
-modificador.capitalizarNovo(ingredientes, "nome");
+let ingredientesOrdenados = modificador.ordenar(ingredientes, 'nome');
+let containerIngredientes = document.querySelector('#container-ingredientes');
+
+for (let ingrediente of ingredientesOrdenados) {
+    let textoHTML = `
+    <div class="ingrediente">
+        <img src="img/${ingrediente.img}">     
+        <p class="nome-ingrediente">${ingrediente.nome}</p>
+    </div>
+    `;
+
+    containerIngredientes.innerHTML += textoHTML;       //+= pra receber TODOS os ingredientes da lista
+}
