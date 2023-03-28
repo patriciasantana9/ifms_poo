4820323377d93adc3957a7f8e8be0766a9afd241
Patrícia Santana <patriciasan3@gmail.com>
2023-03-28 09:34:30 -0300

    limpeza

--- main.js
+++ main.js
@@ -3,16 +3,3 @@ import ingredientes from  "./ingredientes.js";
 //import saladas from  "./saladas.js";
 
 modificador.capitalizarNovo(ingredientes, "nome");
-
-/*
-console.log(modificador);
-console.log(ingredientes);
-console.log(typeof ingredientes);
-*/
-
-/*
-let ingredientes = ["água", "mel", "sal", "mostarda"];
-let resultadoCap = modificador.capitalizar(ingredientes);
-let resultadoOrd = modificador.ordenar(resultadoCap);
-console.log(resultadoCap);
-console.log(resultadoOrd);*/
