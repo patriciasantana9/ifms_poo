681cd9f4b95861c3054c01bb045493c8ff898455
Patrícia Santana <patriciasan3@gmail.com>
2023-03-29 12:47:37 -0300

    saladas

--- saladas.js
+++ saladas.js
@@ -1,7 +1,38 @@
 let saladas = [
     {
-        nome: 'Salada de ',
+        nome: 'Salada 1',
+        ingredientes: 'alface crespa, cebola roxa, pepino, tomate',
         valor: 5
-    }
+    },
+    {
+        nome: 'Salada 2',
+        ingredientes: 'Alface crespa, croutonn, pepino, tomate',
+        valor: 6
+    },
+    {
+        nome: 'Salada 3',
+        ingredientes: 'Cebola roxa, rúcula, tomate cereja',
+        valor: 5
+    },
+    {
+        nome: 'Salada 4',
+        ingredientes: 'Cebola, tomate, pepino, queijo',
+        valor: 5
+    },
+    {
+        nome: 'Salada 5',
+        ingredientes: 'Frango, cebola roxa, agrião, castanhas, tomate cereja',
+        valor: 7
+    },
+    {
+        nome: 'Salada 6',
+        ingredientes: 'tomate, cebolinha, pepino, acelga, abobrinha',
+        valor: 5
+    },
+    {
+        nome: 'Salada 7',
+        ingredientes: 'frango, croutonn, alface crespa, acelga, brócolis',
+        valor: 7
+    },
 ]
 export default saladas;
