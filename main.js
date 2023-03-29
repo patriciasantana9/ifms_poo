fade76b1a681283ad387a2e6f31e8e3f0bbd2db2
Patrícia Santana <patriciasan3@gmail.com>
2023-03-29 09:06:57 -0300

    conclusão-da-base

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import ingredientes from  "./ingredientes.js";
 //import saladas from  "./saladas.js";
 
 let ingredientesOrdenados = modificador.ordenar(ingredientes, 'nome');
-let ingredientesCap = modificador.ordenar(ingredientes, 'nome');
+let ingredientesCap = modificador.capitalizarNovo(ingredientes, 'nome');
 let containerIngredientes = document.querySelector('#container-ingredientes');
 
 for (let ingrediente of ingredientesCap) {
