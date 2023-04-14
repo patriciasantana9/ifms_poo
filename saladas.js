01e2eb43484393b8f3a1b81c17cb05f70a260eec
Patrícia Santana <patriciasan3@gmail.com>
2023-04-14 20:17:52 -0300

    substituição do tipo de objeto

--- saladas.js
+++ saladas.js
@@ -1,46 +1,20 @@
-let saladas = [
-    {
-        nome: 'Salada 1',
-        ingredientes: 'alface crespa, cebola roxa, pepino, tomate',
-        valor: 5,
-        img: 'prato1.png'
-    }, 
-    {
-        nome: 'Salada 2',
-        ingredientes: 'Alface crespa, croutonn, pepino, tomate',
-        valor: 6,
-        img: 'prato2.png'
-    },
-    {
-        nome: 'Salada 3',
-        ingredientes: 'Cebola roxa, rúcula, tomate cereja',
-        valor: 5,
-        img: 'prato3.png'
-    },
-    {
-        nome: 'Salada 4',
-        ingredientes: 'Cebola, tomate, pepino, queijo',
-        valor: 5,
-        img: 'prato4.png'
-    },
-    {
-        nome: 'Salada 5',
-        ingredientes: 'Frango, cebola roxa, agrião, castanhas, tomate cereja',
-        valor: 7,
-        img: 'prato5.png'
-    },
-    {
-        nome: 'Salada 6',
-        ingredientes: 'tomate, cebolinha, pepino, acelga, abobrinha',
-        valor: 5,
-        img: 'prato6.png'
-    },
-    {
-        nome: 'Salada 7',
-        ingredientes: 'frango, croutonn, alface crespa, acelga, brócolis',
-        valor: 7,
-        img: 'prato7.png'
-    },
+function salada(nome, ingredientes, valor, img) {
+    return {
+        nome: nome,
+        ingredientes: ingredientes,
+        valor: valor,
+        img: img
+    }
+}
+
+const saladas = [
+    salada('Salada 1', 'alface crespa, cebola roxa, pepino, tomate', 5, 'prato1.png'),
+    salada('Salada 2', 'Alface crespa, croutonn, pepino, tomate', 6, 'prato2.png'),
+    salada('Salada 3', 'Cebola roxa, rúcula, tomate cereja', 5, 'prato3.png'),
+    salada('Salada 4', 'Cebola, tomate, pepino, queijo', 5, 'prato4.png'),
+    salada('Salada 5', 'Frango, cebola roxa, agrião, castanhas, tomate cereja', 7, 'prato5.png'),
+    salada('Salada 6', 'tomate, cebolinha, pepino, acelga, abobrinha', 5, 'prato6.png'),
+    salada('Salada 7', 'frango, croutonn, alface crespa, acelga, brócolis', 7, 'prato7.png')
 ]
 // console.log(saladas)
 export default saladas;
